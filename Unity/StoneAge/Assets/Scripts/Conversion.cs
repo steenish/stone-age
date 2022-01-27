58819d503d15f9a10e3795ffaa7ef7ca106ebf48
Anders Steen <steenishpost@gmail.com>
2022-01-27 14:08:51 +0100

    refactor using floats instead of doubles

--- Unity/StoneAge/Assets/Scripts/Conversion.cs
+++ Unity/StoneAge/Assets/Scripts/Conversion.cs
@@ -4,16 +4,8 @@ using UnityEngine;
 namespace Utility {
     public class Conversion {
 
-        public static double[,] CreateDoubleBuffer(Texture2D map) {
-            return CreateBuffer(map, (Color pixelColor) => (double) pixelColor.r);
-        }
-
         public static Color[,] CreateColorBuffer(Texture2D map) {
             return CreateBuffer(map, (Color pixelColor) => pixelColor);
-        }
-
-        public static DoubleColor[,] CreateDoubleColorBuffer(Texture2D map) {
-            return CreateBuffer(map, (Color pixelColor) => new DoubleColor(pixelColor.r, pixelColor.g, pixelColor.b));
         }
 
         public static float[,] CreateFloatBuffer(Texture2D map) {
@@ -36,26 +28,15 @@ namespace Utility {
             return CreateTexture(width, height, data, (Color color) => color, textureName);
         }
 
-        public static Texture2D CreateTexture(int width, int height, double[,] data, string textureName = "Texture") {
-            return CreateTexture(width, height, data, (double value) => new Color((float) value, (float) value, (float) value), textureName);
-        }
-
-        public static Texture2D CreateTexture(int width, int height, DoubleColor[,] data, string textureName = "Texture") {
-            return CreateTexture(width, height, data, (DoubleColor color) => new Color((float) color.r, (float) color.g, (float) color.b), textureName);
-        }
-
         public static Texture2D CreateTexture(int width, int height, float[,] data, string textureName = "Texture") {
             return CreateTexture(width, height, data, (float value) => new Color(value, value, value), textureName);
         }
 
-        public static Texture2D CreateTexture(int width, int height, Vector3[,] data, string textureName = "Texture") {
-            return CreateTexture(width, height, data, (Vector3 value) => new Color(value.x * 0.5f + 0.5f, value.y * 0.5f + 0.5f, value.z * 0.5f + 0.5f), textureName);
-        }
-
         private static Texture2D CreateTexture<T>(int width, int height, T[,] data, Func<T, Color> colorExtractionFunction, string textureName = "Texture") {
-            Texture2D texture = new Texture2D(width, height);
-            texture.name = textureName;
-            Color[] colors = new Color[width * height];
+			Texture2D texture = new Texture2D(width, height) {
+				name = textureName
+			};
+			Color[] colors = new Color[width * height];
 
             for (int y = 0; y < height; ++y) {
                 for (int x = 0; x < width; ++x) {
@@ -82,14 +63,6 @@ namespace Utility {
 			}
 
             return extractedLayer;
-        }
-
-        public static void FillDoubleBufferLayer(Texture2D map, ref double[,,] layers, int layerIndex) {
-            FillBufferLayer(map, ref layers, layerIndex, (Color pixelColor) => (double) pixelColor.r);
-        }
-
-        public static void FillColorBufferLayer(Texture2D map, ref DoubleColor[,,] layers, int layerIndex) {
-            FillBufferLayer(map, ref layers, layerIndex, (Color pixelColor) => new DoubleColor(pixelColor.r, pixelColor.g, pixelColor.b));
         }
 
         public static void FillFloatBufferLayer(Texture2D map, ref float[,,] layers, int layerIndex) {
--- Unity/StoneAge/Assets/Scripts/Textures.cs
+++ Unity/StoneAge/Assets/Scripts/Textures.cs
@@ -12,20 +12,12 @@ namespace Utility {
 			DrawTexture(Conversion.CreateTexture(width, height, data, textureName));
 		}
 
-		public static void DrawTexture(int width, int height, double[,] data, string textureName = "Debug Texture") {
-			DrawTexture(Conversion.CreateTexture(width, height, data, textureName));
-		}
-
-		public static void DrawTexture(int width, int height, DoubleColor[,] data, string textureName = "Debug Texture") {
-			DrawTexture(Conversion.CreateTexture(width, height, data, textureName));
-		}
-
 		public static void DrawTexture(Texture2D texture) {
 			RawImage outputImage = GameObject.Find("DebugTextureImage").GetComponent<RawImage>();
 			outputImage.texture = texture;
 		}
 
-		public static Color[,] OverlaySediment(Color[,] albedo, double[,] sediment, Gradient sedimentGradient) {
+		public static Color[,] OverlaySediment(Color[,] albedo, float[,] sediment, Gradient sedimentGradient) {
 			int width = sediment.GetLength(1);
 			int height = sediment.GetLength(0);
 
@@ -34,7 +26,7 @@ namespace Utility {
 			for (int y = 0; y < height; ++y) {
 				for (int x = 0; x < width; ++x) {
 					Color sedimentColor = sedimentGradient.Evaluate(Random.value);
-					result[x, y] = BlendColors(sedimentColor, albedo[x, y], (float) sediment[x, y]);
+					result[x, y] = BlendColors(sedimentColor, albedo[x, y], sediment[x, y]);
 				}
 			}
 
