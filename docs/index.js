ab6d5793920437128d4f7aa2f9869e4432d75edd
Anders Steen <steenishpost@gmail.com>
2022-03-23 09:59:20 +0100

    upload test version

--- docs/index.js
+++ docs/index.js
@@ -1,87 +1,92 @@
+// const trials = [
+//     ["S1C1C1", "S1C1C2"],
+//     ["S1C1C1", "S1C1C3"],
+//     ["S1C1C1", "S1C2C1"],
+//     ["S1C1C1", "S1C3C1"],
+//     ["S1C1C1", "S2C1C1"],
+//     ["S1C1C1", "S3C1C1"],
+//     ["S1C1C2", "S1C1C3"],
+//     ["S1C1C2", "S1C2C2"],
+//     ["S1C1C2", "S1C3C2"],
+//     ["S1C1C2", "S2C1C2"],
+//     ["S1C1C2", "S3C1C2"],
+//     ["S1C1C3", "S1C2C3"],
+//     ["S1C1C3", "S1C3C3"],
+//     ["S1C1C3", "S2C1C3"],
+//     ["S1C1C3", "S3C1C3"],
+//     ["S1C2C1", "S1C2C2"],
+//     ["S1C2C1", "S1C2C3"],
+//     ["S1C2C1", "S1C3C1"],
+//     ["S1C2C1", "S2C2C1"],
+//     ["S1C2C1", "S3C2C1"],
+//     ["S1C2C2", "S1C2C3"],
+//     ["S1C2C2", "S1C3C2"],
+//     ["S1C2C2", "S2C2C2"],
+//     ["S1C2C2", "S3C2C2"],
+//     ["S1C2C3", "S1C3C3"],
+//     ["S1C2C3", "S2C2C3"],
+//     ["S1C2C3", "S3C2C3"],
+//     ["S1C3C1", "S1C3C2"],
+//     ["S1C3C1", "S1C3C3"],
+//     ["S1C3C1", "S2C3C1"],
+//     ["S1C3C1", "S3C3C1"],
+//     ["S1C3C2", "S1C3C3"],
+//     ["S1C3C2", "S2C3C2"],
+//     ["S1C3C2", "S3C3C2"],
+//     ["S1C3C3", "S2C3C3"],
+//     ["S2C1C1", "S2C1C2"],
+//     ["S2C1C1", "S2C1C3"],
+//     ["S2C1C1", "S2C2C1"],
+//     ["S2C1C1", "S2C3C1"],
+//     ["S2C1C1", "S3C1C1"],
+//     ["S2C1C2", "S2C1C3"],
+//     ["S2C1C2", "S2C2C2"],
+//     ["S2C1C2", "S2C3C2"],
+//     ["S2C1C2", "S3C1C2"],
+//     ["S2C1C3", "S2C2C3"],
+//     ["S2C1C3", "S2C3C3"],
+//     ["S2C1C3", "S3C1C3"],
+//     ["S2C2C1", "S2C2C2"],
+//     ["S2C2C1", "S2C2C3"],
+//     ["S2C2C1", "S2C3C1"],
+//     ["S2C2C1", "S3C2C1"],
+//     ["S2C2C2", "S2C2C3"],
+//     ["S2C2C2", "S2C3C2"],
+//     ["S2C2C2", "S3C2C2"],
+//     ["S2C2C3", "S2C3C3"],
+//     ["S2C2C3", "S3C2C3"],
+//     ["S2C3C1", "S2C3C2"],
+//     ["S2C3C1", "S2C3C3"],
+//     ["S2C3C1", "S3C3C1"],
+//     ["S2C3C2", "S2C3C3"],
+//     ["S2C3C2", "S3C3C2"],
+//     ["S3C1C1", "S3C1C2"],
+//     ["S3C1C1", "S3C1C3"],
+//     ["S3C1C1", "S3C2C1"],
+//     ["S3C1C1", "S3C3C1"],
+//     ["S3C1C2", "S3C1C3"],
+//     ["S3C1C2", "S3C2C2"],
+//     ["S3C1C2", "S3C3C2"],
+//     ["S3C1C3", "S3C2C3"],
+//     ["S3C2C1", "S3C2C2"],
+//     ["S3C2C1", "S3C2C3"],
+//     ["S3C2C1", "S3C3C1"],
+//     ["S3C2C2", "S3C2C3"],
+//     ["S3C2C2", "S3C3C2"],
+//     ["S3C3C1", "S3C3C2"]
+// ];
 const trials = [
     ["S1C1C1", "S1C1C2"],
-    ["S1C1C1", "S1C1C3"],
-    ["S1C1C1", "S1C2C1"],
-    ["S1C1C1", "S1C3C1"],
-    ["S1C1C1", "S2C1C1"],
-    ["S1C1C1", "S3C1C1"],
-    ["S1C1C2", "S1C1C3"],
-    ["S1C1C2", "S1C2C2"],
-    ["S1C1C2", "S1C3C2"],
-    ["S1C1C2", "S2C1C2"],
-    ["S1C1C2", "S3C1C2"],
-    ["S1C1C3", "S1C2C3"],
-    ["S1C1C3", "S1C3C3"],
-    ["S1C1C3", "S2C1C3"],
-    ["S1C1C3", "S3C1C3"],
-    ["S1C2C1", "S1C2C2"],
-    ["S1C2C1", "S1C2C3"],
-    ["S1C2C1", "S1C3C1"],
-    ["S1C2C1", "S2C2C1"],
-    ["S1C2C1", "S3C2C1"],
-    ["S1C2C2", "S1C2C3"],
-    ["S1C2C2", "S1C3C2"],
-    ["S1C2C2", "S2C2C2"],
-    ["S1C2C2", "S3C2C2"],
-    ["S1C2C3", "S1C3C3"],
-    ["S1C2C3", "S2C2C3"],
-    ["S1C2C3", "S3C2C3"],
-    ["S1C3C1", "S1C3C2"],
-    ["S1C3C1", "S1C3C3"],
-    ["S1C3C1", "S2C3C1"],
-    ["S1C3C1", "S3C3C1"],
-    ["S1C3C2", "S1C3C3"],
-    ["S1C3C2", "S2C3C2"],
-    ["S1C3C2", "S3C3C2"],
-    ["S1C3C3", "S2C3C3"],
-    ["S2C1C1", "S2C1C2"],
-    ["S2C1C1", "S2C1C3"],
-    ["S2C1C1", "S2C2C1"],
-    ["S2C1C1", "S2C3C1"],
-    ["S2C1C1", "S3C1C1"],
-    ["S2C1C2", "S2C1C3"],
-    ["S2C1C2", "S2C2C2"],
-    ["S2C1C2", "S2C3C2"],
-    ["S2C1C2", "S3C1C2"],
-    ["S2C1C3", "S2C2C3"],
-    ["S2C1C3", "S2C3C3"],
-    ["S2C1C3", "S3C1C3"],
-    ["S2C2C1", "S2C2C2"],
-    ["S2C2C1", "S2C2C3"],
-    ["S2C2C1", "S2C3C1"],
-    ["S2C2C1", "S3C2C1"],
-    ["S2C2C2", "S2C2C3"],
-    ["S2C2C2", "S2C3C2"],
-    ["S2C2C2", "S3C2C2"],
-    ["S2C2C3", "S2C3C3"],
-    ["S2C2C3", "S3C2C3"],
-    ["S2C3C1", "S2C3C2"],
-    ["S2C3C1", "S2C3C3"],
-    ["S2C3C1", "S3C3C1"],
-    ["S2C3C2", "S2C3C3"],
-    ["S2C3C2", "S3C3C2"],
-    ["S3C1C1", "S3C1C2"],
-    ["S3C1C1", "S3C1C3"],
-    ["S3C1C1", "S3C2C1"],
-    ["S3C1C1", "S3C3C1"],
-    ["S3C1C2", "S3C1C3"],
-    ["S3C1C2", "S3C2C2"],
-    ["S3C1C2", "S3C3C2"],
-    ["S3C1C3", "S3C2C3"],
-    ["S3C2C1", "S3C2C2"],
-    ["S3C2C1", "S3C2C3"],
-    ["S3C2C1", "S3C3C1"],
-    ["S3C2C2", "S3C2C3"],
-    ["S3C2C2", "S3C3C2"],
-    ["S3C3C1", "S3C3C2"]
+    ["S1C1C1", "S1C1C3"]
 ];
-const trialOrder = trials.keys();
+const trialOrder = [...trials.keys()];
 shuffleArray(trialOrder);
 const numTrials = trials.length;
-let currentTrialNumber = -1;
+let currentTrialIndex = -1;
 const pageDelay = 500;//0;
 const trialDelay = 100;//0;
 let startTime = new Date();
+let results = []
 
 setTimeout(() => $("#continueButton").prop("disabled", false), pageDelay);
 
@@ -106,31 +111,12 @@ function startButtonPressed() {
 function startTrials() {
     $("#trialPage").show();
     setButtonEnableTimer("realismButton", trialDelay);
-    
     nextTrial();
 }
 
 function realismSubmit(button) { //TODO
-    $("#realismQuestion").hide();
-    $("#appealQuestion").show();
-    
-    setButtonEnableTimer("appealButton", trialDelay);
-    $(".realismButton").hide();
-    $(".appealButton").show();
-
-    realismResults.push(getTrialResult(button));
-}
-
-function appealSubmit(button) { //TODO
-    $("#realismQuestion").show();
-    $("#appealQuestion").hide();
-    
     setButtonEnableTimer("realismButton", trialDelay);
-    $(".realismButton").show();
-    $(".appealButton").hide();
-
-    appealResults.push(getTrialResult(button));
-
+    results.push(getTrialResult(button));
     nextTrial();
 }
 
@@ -141,21 +127,25 @@ function getTrialResult(button) { //TODO
     } else {
         imageString = $("#rightImage").attr("src");
     }
-    return `${imageString.substring(4, imageString.length - 4)}(${button})`;
+    return {
+        trialNum: trialOrder[currentTrialIndex],
+        selectedImage: imageString.substring(4, imageString.length - 4),
+        selectedButton: button
+    }
 }
 
 function nextTrial() { //TODO
-    currentTrialNumber += 1;
-    if (currentTrialNumber >= numTrials) {
+    currentTrialIndex += 1;
+    if (currentTrialIndex >= numTrials) {
         finishTrials();
     } else {
-        $("#trialNumber").text(`Trial ${currentTrialNumber + 1}`)
+        $("#trialNumber").text(`Trial ${currentTrialIndex + 1}`)
         if (Math.random() < 0.5) {
-            $("#leftImage").attr("src", `img/${trialOrder[currentTrialNumber]}/NL.png`);
-            $("#rightImage").attr("src", `img/${trialOrder[currentTrialNumber]}/WL.png`);
+            $("#leftImage").attr("src", `img/${trials[trialOrder[currentTrialIndex]][0]}.png`);
+            $("#rightImage").attr("src", `img/${trials[trialOrder[currentTrialIndex]][1]}.png`);
         } else {
-            $("#leftImage").attr("src", `img/${trialOrder[currentTrialNumber]}/WL.png`);
-            $("#rightImage").attr("src", `img/${trialOrder[currentTrialNumber]}/NL.png`);
+            $("#leftImage").attr("src", `img/${trials[trialOrder[currentTrialIndex]][1]}.png`);
+            $("#rightImage").attr("src", `img/${trials[trialOrder[currentTrialIndex]][0]}.png`);
         }
     }
 }
@@ -166,27 +156,28 @@ function finishTrials() {
 }
 
 function verifyAndGatherData() {
-    let data = {
-        gender: $("select[name=gender]").find(":selected").text(),
-        age: parseInt($("input[name=age]").val(), 10),
-        education: $("select[name=education]").find(":selected").text(),
-        country: $("input[name=country]").val(),
-        experience: $("select[name=experience]").find(":selected").text(),
-        vision: $("select[name=vision]").find(":selected").text(),
-        comments: $("textarea[name=comments]").val()
-    }
+    let potentialAge  = parseInt($("input[name=age]").val(), 10);
+    let potentialCountry = $("input[name=country]").val();
     
-    if (Number.isInteger(age) && country) {
-        data.completionCode = generateCompletionCode(10);
-        data.duration = 0.001 * (new Date() - startTime);
-        
-        // TODO
-        // for (var i = 0; i < realismResults.length; ++i) {
-        //     $(`input[name=Trial${i + 1}]`).val(`realism[${realismResults[i]}], appeal[${appealResults[i]}]`);
-        // }
+    if (Number.isInteger(potentialAge) && potentialCountry) {
+        let data = {
+            gender: $("select[name=gender]").find(":selected").text(),
+            age: potentialAge,
+            education: $("select[name=education]").find(":selected").text(),
+            country: potentialCountry,
+            experience: $("select[name=experience]").find(":selected").text(),
+            vision: $("select[name=vision]").find(":selected").text(),
+            comments: $("textarea[name=comments]").val(),
+            completionCode: generateCompletionCode(10),
+            duration: 0.001 * (new Date() - startTime),
+            trialResults: results
+        }
 
-        alert(`Your Mechanical Turk completion code is: ${data.completionCode}`);
+        var tab = window.open('about:blank', '_blank');
+        tab.document.write(`Your Mechanical Turk completion code is: ${data.completionCode}`); // where 'html' is a variable containing your HTML
+        tab.document.close(); // to finish loading the page
 
+        $("input[name=Data]").val(JSON.stringify(data));
         $("#dataForm").submit();
         $("#demographicsPage").hide();
     } else {
